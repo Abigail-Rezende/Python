739a2f111bbc237eb441b3ebb9fcf5fd2a01c89b
Abigail-Rezende <abigailcrezende@yahoo.com.br>
2018-11-08 18:21:57 -0200

    Formulário

---
+++ Formulario.py
@@ -0,0 +1,14 @@
+'''
+Exercício: Faça um formulário que pergunte
+o nome, cpf, endereço, idade, altura e telefone.
+Imprma os resultados em um relatório organizado
+'''
+
+nome = input('Digite seu nome: ')
+cpf = input('Digite seu CPF: ')
+ender = input('Digite seu endereço: ')
+idade = input('Digite sua idade: ')
+altura = input('Digite sua altura: ')
+tele = input('Digite o número de seu telfone: ')
+
+print('\nNome:',nome, '\nCPF:', cpf, '\Endereço:', ender, '\nIdade:', idade, '\nAltura:', altura, '\nTelefone:', tele)
